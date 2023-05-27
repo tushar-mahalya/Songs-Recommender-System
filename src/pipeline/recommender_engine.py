84aa441c08a288ce52183b27002b8de415294406
Tushar Sharma <tusharmahalya@gmail.com>
2023-05-27 16:18:35 +0530

    recommender engine update

--- src/pipeline/recommender_engine.py
+++ src/pipeline/recommender_engine.py
@@ -1,11 +1,17 @@
 import os
+import pandas as pd
 import sys
 import json
 from src.exception import CustomException
 from src.logger import logging
+from config import ROOT_DIR
 
 from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    data_path: str = '/data/[Spotify]_Billboard_Hot100_Songs_1946-2022.csv'
+    data_path: str = ROOT_DIR + '/data/[Spotify]_Billboard_Hot100_Songs_1946-2022.csv'
+if __name__ == '__main__':
+    config = DataIngestionConfig()
+    data = pd.read_csv(config.data_path)
+    print('Data Read')

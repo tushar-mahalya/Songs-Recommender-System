bb5c7295f367aa20b35b3b6ceb43f907c0d6090f
Tushar Sharma <tusharmahalya@gmail.com>
2023-05-28 16:11:53 +0530

    data wrangling functions update #2

--- src/pipeline/data_wrangling.py
+++ src/pipeline/data_wrangling.py
@@ -9,18 +9,20 @@ from src.components.sentiment import Sentiment_Features
 from src.exception import CustomException
 from src.logger import logging
 
+
 @dataclass
 class DataWranglingConfig:
     data_path: str = 'data/[Spotify]_Billboard_Hot100_Songs_1946-2022.csv'
-    
+
+
 class DataPreprocessing:
     def __init__(self):
         config = DataWranglingConfig()
         self._data = pd.read_csv(config.data_path)
         logging.info('Data read successfully from /data directory.')
 
-
-    def data_preprocessing(self, data_df: pd.DataFrame):
+    @staticmethod
+    def data_preprocessing(data_df: pd.DataFrame):
         try:
             logging.info('Data Preprocessing started.')
             prep_df = combineSongArtist(data_df)
@@ -33,22 +35,22 @@ class DataPreprocessing:
             subject_ohe = OHE_Column(subject_df, 'subjectivity', 'Subjectivity') * 0.5
             polar_ohe = OHE_Column(polar_df, 'polarity', 'Polarity') * 0.5
             num_feats = ['Popularity', 'Acousticness', 'Danceability', 'Energy', 'Instrumentalness',
-                         'Liveness', 'Loudness', 'Speechiness','Tempo', 'Valence']
+                         'Liveness', 'Loudness', 'Speechiness', 'Tempo', 'Valence']
             scaled_feats = Standardize_Features(prep_df, num_feats)
             final_df = pd.concat([genre_df, subject_ohe, polar_ohe, key_ohe, mode_ohe,
-                                  time_sig_ohe, scaled_feats], axis = 1)
+                                  time_sig_ohe, scaled_feats], axis=1)
             logging.info('Data Preprocessing completed.')
             return prep_df, final_df
-        
+
         except Exception as e:
-            raise CustomException(e,sys) 
+            raise CustomException(e, sys)
 
     def get_preprocessed_data(self):
         try:
             songs_data, feats_data = self.data_preprocessing(self._data)
-            songs_data.to_csv('artifacts/[Songs]_Preprocessed_Data.csv', index = False)
-            feats_data.to_csv('artifacts/[Features]_Preprocessed_Data.csv', index = False)
+            songs_data.to_csv('artifacts/[Songs]_Preprocessed_Data.csv', index=False)
+            feats_data.to_csv('artifacts/[Features]_Preprocessed_Data.csv', index=False)
             logging.info('Preprocessed Data and Features Data is stored in /artifacts directory.')
             return songs_data, feats_data
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)

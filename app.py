ae4c2d07cba26c579480b2b49848a6c71fb62968
Tushar Sharma <tusharmahalya@gmail.com>
2023-06-01 12:14:22 +0000

    added new footer

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ casette = 'https://www.scdn.co/i/500/cassette.svg'
 
 # Removing the Hamberger menu
 hide_menu_style = """<style> #MainMenu {visibility: hidden;}
-header {visibility: hidden;} footer {visibility: hidden;}</style>"""
+header {visibility: hidden;}</style>"""
 
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
